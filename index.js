746ec859b3ff456d568c86a33861680df33153f3
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-20 10:09:17 +0300

    modified index.js to be able to fetch an api

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((books) => renderBooks(books));
 }
+
+function renderBooks(books) {
+  const main = document.querySelector("main");
+
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name;
+    main.appendChild(h2);
+  });
+}
+
+// Call fetchBooks when the page loads
+document.addEventListener("DOMContentLoaded", fetchBooks);
 
 function renderBooks(books) {
   const main = document.querySelector('main');
